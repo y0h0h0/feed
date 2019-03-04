e7f5935255abcc531d87d16237326e43b29163dc
Tom Teaser <y0h0h0@yandex.ru>
2019-03-04 15:30:11 +0300

    recoverdb

--- server.js
+++ server.js
@@ -11,39 +11,32 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use('*', (req, res) => {
-  res.status(200).send('works');
+app.use('/', express.static('front/build'));
+
+app.use('/api/auth/', require('./api_controllers/auth.js'));
+app.use('/api/posts/', require('./api_controllers/posts.js'));
+
+app.use('/api/*', (req, res) => {
+  console.log(req)
+  res.status(404).send('Wrong api endpoint');
 });
 
-// app.use('/', express.static('front/build'));
-//
-// app.use('/api/auth/', require('./api_controllers/auth.js'));
-// app.use('/api/posts/', require('./api_controllers/posts.js'));
-//
-// app.use('/api/*', (req, res) => {
-//   console.log(req)
-//   res.status(404).send('Wrong api endpoint');
-// });
-//
-// // Front-end SPA
-// app.use('*', (req, res) => {
-//   console.log(chalk.green('SENDING A STAR'))
-//   res.sendFile(__dirname + '/front/build/index.html');
-// });
-//
-// var db = require('./db');
 
-// db.connect((err)=>{
-//   if(err) return err;
-//
-//   app.listen(PORT, () => {
-//     console.clear();
-//     console.log(chalk.bgMagenta.black(`Listening on ${ PORT }` ) + chalk.bgBlue.black(`==============` )    );
-//   })
-//
-// })
 
-app.listen(PORT, () => {
-  console.clear();
-  console.log(chalk.bgMagenta.black(`Listening on ${ PORT }` ) + chalk.bgBlue.black(`==============` )    );
+// Front-end SPA
+app.use('*', (req, res) => {
+  console.log(chalk.green('SENDING A STAR'))
+  res.sendFile(__dirname + '/front/build/index.html');
+});
+
+var db = require('./db');
+
+db.connect((err)=>{
+  if(err) return err;
+
+  app.listen(PORT, () => {
+    console.clear();
+    console.log(chalk.bgMagenta.black(`Listening on ${ PORT }` ) + chalk.bgBlue.black(`==============` )    );
+  })
+
 })
