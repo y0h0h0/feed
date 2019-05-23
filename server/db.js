b6f25d3c4b1e367535beb2eab84f238dab0e8e56
Tom Teaser <y0h0h0@yandex.ru>
2019-05-23 12:18:26 +0300

    some changes around ssh

--- server/db.js
+++ server/db.js
@@ -14,7 +14,8 @@ var sshConf = {
   port: process.env.SSH_PORT,
   username: process.env.SSH_USER,
   password: process.env.SSH_PASS,
-  keepaliveInterval:20000
+  keepaliveInterval:20000,
+  readyTimeout:30000
 };
 var sqlConf = {
   user: process.env.SQL_USER,
